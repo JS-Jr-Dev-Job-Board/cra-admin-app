b2b26020e459274d37bfc14998c104606491be14
unknown <tompsherman@gmail.com>
2021-01-23 07:53:10 -0500

    initial app & style

--- admin/src/App.js
+++ admin/src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Welcome to the Central Nervous System
+      </h1>
+      <button>download experimental malware</button>
+      <button>logout</button>
     </div>
   );
 }
