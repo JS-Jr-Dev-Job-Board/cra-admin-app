af470f087f7a5a4f47522839f26677a529cf60bc
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro.local>
2021-01-24 22:50:17 -0500

    added to app.js only to see it rendering

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 import './App.css';
+import Dashboard from './components/Dashboard/Dashboard'
 import LoginBtn from './components/OaLogic/LoginBtn'
 import LogoutBtn from './components/OaLogic/LogoutBtn'
 import Profile from './components/Profile';
 import { useAuth0 } from '@auth0/auth0-react';
+
+
 
 function App() {
   const { isLoading } = useAuth0()
@@ -17,7 +20,9 @@ function App() {
       </h1>
       <LoginBtn />
       <LogoutBtn />
+      <Dashboard />
       <Profile />
+
     </div>
   );
 }
