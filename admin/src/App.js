47818b56d82842a79093b20ac586d8fd735e20a1
BrianReisman <brianreisman@protonmail.com>
2021-03-13 23:24:33 -0500

    added NavLink and nav. Put EndpointPage in Route

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
-import {Route} from 'react-router-dom';
+import { Route, NavLink } from "react-router-dom";
 
 import Signin from "./components/Signin/Signin";
-import './App.css';
-import LoginBtn from './components/OaLogic/LoginBtn'
-import LogoutBtn from './components/OaLogic/LogoutBtn'
-import Profile from './components/Profile';
-import EndpointPage from './components/EndpointPage/EndpointPage';
+import "./App.css";
+// import LoginBtn from "./components/OaLogic/LoginBtn";
+// import LogoutBtn from "./components/OaLogic/LogoutBtn";
+import Profile from "./components/Profile";
+import EndpointPage from "./components/EndpointPage/EndpointPage";
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/signin">Sign In</NavLink>
+        <NavLink to="/endpointPage">Endpoint Page</NavLink>
+      </nav>
       <h1>Welcome to the Central Nervous System</h1>
-      <LoginBtn />
-      <LogoutBtn />
+      {/* <LoginBtn /> */}
+      {/* <LogoutBtn /> */}
       <Profile />
-      <Route path='/signin' component={Signin}/>
-      <EndpointPage/>
+      <Route path="/signin" component={Signin} />
+      <Route path="/endpointPage" component={EndpointPage} />
     </div>
   );
 }
